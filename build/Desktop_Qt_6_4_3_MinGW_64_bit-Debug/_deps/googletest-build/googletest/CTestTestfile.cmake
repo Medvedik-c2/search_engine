905fe60e312caf4a2044e071853b1a43939f5445
Dmitrii Ni <medvedik-c2@mail.ru>
2024-04-17 09:50:25 +1000

    delete build

--- build/Desktop_Qt_6_4_3_MinGW_64_bit-Debug/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/develop/search_engine/build/Desktop_Qt_6_4_3_MinGW_64_bit-Debug/_deps/googletest-src/googletest
-# Build directory: C:/develop/search_engine/build/Desktop_Qt_6_4_3_MinGW_64_bit-Debug/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/Desktop_Qt_6_4_3_MinGW_64_bit-Debug/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/develop/search_engine/src
-# Build directory: C:/develop/search_engine/build/Desktop_Qt_6_4_3_MinGW_64_bit-Debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/develop/search_engine/build/Desktop_Qt_6_4_3_MinGW_64_bit-Debug/src/search_engine[1]_include.cmake")
